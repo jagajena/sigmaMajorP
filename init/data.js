9414a2269cd13f26b8b47991f59ca9f741c393a2
jagannath jena <jenajagannath388@gmail.com>
2025-04-28 08:48:27 +0530

    Add Project Files

---
+++ init/data.js
@@ -0,0 +1,97 @@
+const sampleListings = [
+  {
+    title: "Seaside Villa",
+    description: "A luxurious villa with a stunning view of the ocean.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price: 4500,
+    location: "Malibu, CA",
+    country: "USA"
+  },
+  {
+    title: "Mountain Retreat",
+    description: "A peaceful retreat in the heart of the mountains.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price: 2000,
+    location: "Aspen, CO",
+    country: "USA"
+  },
+  {
+    title: "Urban Apartment",
+    description: "A modern apartment in the city center with all amenities.",
+    image: {
+      filename:  "listingimage",
+      url: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price: 1500,
+    location: "New York, NY",
+    country: "USA"
+  },
+  {
+    title: "Countryside Cottage",
+    description: "A cozy cottage surrounded by nature.",
+    image: {
+      filename:  "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price: 1200,
+    location: "Cotswolds",
+    country: "UK"
+  },
+  {
+    title: "Beachfront Bungalow",
+    description: "A charming bungalow right by the beach.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661962841993-99a07c27c9f4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvdXNlfGVufDB8fDB8fHww"
+    },
+    price: 3200,
+    location: "Gold Coast",
+    country: "Australia"
+  },
+  {
+    title: "Darzile Apartment",
+    description: "A modern apartment in the city center with all amenities. this is a test with a lost",
+    image: {
+      filename:  "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661920539164-55a9b1e44eb1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI3fHx8ZW58MHx8fHx8"
+    },
+    price: 1500,
+    location: "New York, NY",
+    country: "USA"
+  },
+  {
+    title: "Countryside Cottage",
+    description: "A cozy cottage surrounded by nature.",
+    image: {
+      filename:  "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1736194027564-74e1a686babd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDMzfHx8ZW58MHx8fHx8"
+    },
+    price: 1200,
+    location: "Cotswolds",
+    country: "UK"
+  },
+  {
+    title: "Beachfront Bungalow",
+    description: "A charming bungalow right by the beach.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1736194025882-1aad786e38ec?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDM5fHx8ZW58MHx8fHx8"
+    },
+    price: 3200,
+    location: "Gold Coast",
+    country: "Australia"
+  }
+];
+
+ module.exports = { data: sampleListings };
+
+
+
+
+ 
